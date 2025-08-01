73c293a316c8a6c7cf295bcc7d03507fe1fe9afc
Mitch Murphy <mitch.murphy@gmail.com>
2025-08-01 12:19:16 -0400

    chore(main): release 0.3.0

--- src/stockula/__init__.py
+++ src/stockula/__init__.py
@@ -28,7 +28,7 @@ from .data import DataFetcher
 from .forecasting import StockForecaster
 from .technical_analysis import TechnicalIndicators
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "DataFetcher",
